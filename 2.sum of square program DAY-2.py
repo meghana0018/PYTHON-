a73b3b4d087d6d7703bc99b31dff889c811dcd97
mallipudi meghana <114044829+meghana0018@users.noreply.github.com>
2022-10-04 08:48:44 +0530

    Add files via upload

---
+++ 2.sum of square program DAY-2.py
@@ -0,0 +1,12 @@
+1[8,7,6,9,13,23,42,77,23]
+def sumsquare(1):
+    odd=0
+    even=0
+    for i in 1:
+        if i%2==0:
+            even=even+i**2
+            else
+            odd=odd+**2
+            1=[odd,even]
+            return(1)
+        print(sumsquare(1))
